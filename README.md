27091232cde4a4050b05c85062480fc79958512e
gmgmgm <gmgmgm006@gmail.com>
2019-02-17 23:35:42 +0900

    add ABC088B

--- README.md
+++ README.md
@@ -8,6 +8,6 @@ ABC 081 A B
 ABC 083   B  
 ABC 086 A  
 ABC 087   B  
-
+ABC 088   B
 ## Joined Question
 ABC 118 A B(2) C Dx
---
+++ practice/ABC088B.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int n, ans = 0;
+	cin >> n;
+	vector<int> a(n, 0);
+	for (int i = 0;i < n;i++)
+	{
+		cin >> a[i];
+	}
+	sort(a.begin(), a.end(),greater<int>());
+	for (int i = 0;i < n;i++)
+	{
+		if (i % 2 == 0)
+		{
+			ans += a[i];
+		}
+		else
+		{
+			ans -= a[i];
+		}
+	}
+
+	cout << ans << endl;
+	return 0;
+}
